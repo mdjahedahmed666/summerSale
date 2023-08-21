cea8c49d2e05933a104be24db908224c5c18107f
Jahed <mdjahedahmed666@gmail.com>
2023-08-21 11:54:08 +0100

    all functionality added

--- script.js
+++ script.js
@@ -15,22 +15,168 @@ function getTotalPrice(id){
   const totalPrice = parseFloat(totalPriceElString);
 return totalPrice;
 }
+let itemCounter = 0;
+const addedLi = [];
+
+function showName(id,name){
+  console.log(id);
+  console.log(name);
+    // get the Ol to show name
+    const showName = document.getElementById(id);
+    const li = document.createElement("li");
+    itemCounter++;
+    li.textContent = `${itemCounter}. ${name}`;
+    li.style.fontWeight = "bold";
+    li.style.fontSize = "24px";
+    li.style.marginBottom = "12px";
+    showName.appendChild(li);
+    addedLi.push(li);
+}
 
 function handleKAccessories() {
-  let itemCounter = 0;
 const name = getAccessoriesName("kAccessories1");
+showName("showName",name);
 
-  // get the Ol to show name
-  const showName = document.getElementById("showName");
-  const li = document.createElement("li");
-  li.textContent = `${itemCounter + 1}. ${name}`;
-  li.style.fontWeight = "medium";
-  li.style.fontSize = "24px";
-  li.style.marginBottom = "12px";
-  showName.appendChild(li);
-  itemCounter++;
 //get the price of the accessories
 const price = getAccessoriesPrice("kAPrice");
+
+//get the total price 
+const totalPrice = getTotalPrice("totalPrice");
+
+const sum = totalPrice + price;
+const totalPriceEl = document.getElementById("totalPrice");
+totalPriceEl.innerText = sum;
+
+  //   Get the Purchase button ref
+  const purchaseBtn = document.getElementById("purchaseButton");
+  if (sum > 0) {
+    purchaseBtn.classList.remove("btn-disabled");
+    purchaseBtn.classList.add("btn-secondary");
+  }
+  //   Get the Apply button ref
+  const applyBtn = document.getElementById("applyButton");
+  if (sum >= 200) {
+    applyBtn.classList.remove("btn-disabled");
+    applyBtn.classList.add("btn-secondary");
+  }
+}
+
+
+function handleCardHomeCooker() {
+const name = getAccessoriesName("cooker");
+showName("showName",name);
+
+//get the price of the accessories
+const price = getAccessoriesPrice("cookerPrice");
+
+//get the total price 
+const totalPrice = getTotalPrice("totalPrice");
+
+const sum = totalPrice + price;
+const totalPriceEl = document.getElementById("totalPrice");
+totalPriceEl.innerText = sum;
+
+  //   Get the Purchase button ref
+  const purchaseBtn = document.getElementById("purchaseButton");
+  if (sum > 0) {
+    purchaseBtn.classList.remove("btn-disabled");
+    purchaseBtn.classList.add("btn-secondary");
+  }
+  //   Get the Apply button ref
+  const applyBtn = document.getElementById("applyButton");
+  if (sum >= 200) {
+    applyBtn.classList.remove("btn-disabled");
+    applyBtn.classList.add("btn-secondary");
+  }
+}
+
+function handleKAccessories2() {
+const name = getAccessoriesName("kAccessories2");
+showName("showName",name);
+
+//get the price of the accessories
+const price = getAccessoriesPrice("kAPrice2");
+
+//get the total price 
+const totalPrice = getTotalPrice("totalPrice");
+
+const sum = totalPrice + price;
+const totalPriceEl = document.getElementById("totalPrice");
+totalPriceEl.innerText = sum;
+
+  //   Get the Purchase button ref
+  const purchaseBtn = document.getElementById("purchaseButton");
+  if (sum > 0) {
+    purchaseBtn.classList.remove("btn-disabled");
+    purchaseBtn.classList.add("btn-secondary");
+  }
+  //   Get the Apply button ref
+  const applyBtn = document.getElementById("applyButton");
+  if (sum >= 200) {
+    applyBtn.classList.remove("btn-disabled");
+    applyBtn.classList.add("btn-secondary");
+  }
+}
+
+function handleCardCap() {
+const name = getAccessoriesName("capName");
+showName("showName",name);
+const price = getAccessoriesPrice("capPrice");
+
+//get the total price 
+const totalPrice = getTotalPrice("totalPrice");
+
+const sum = totalPrice + price;
+const totalPriceEl = document.getElementById("totalPrice");
+totalPriceEl.innerText = sum;
+
+  //   Get the Purchase button ref
+  const purchaseBtn = document.getElementById("purchaseButton");
+  if (sum > 0) {
+    purchaseBtn.classList.remove("btn-disabled");
+    purchaseBtn.classList.add("btn-secondary");
+  }
+  //   Get the Apply button ref
+  const applyBtn = document.getElementById("applyButton");
+  if (sum >= 200) {
+    applyBtn.classList.remove("btn-disabled");
+    applyBtn.classList.add("btn-secondary");
+  }
+}
+
+function handleCardJersey() {
+const name = getAccessoriesName("jerseyName");
+showName("showName",name);
+const price = getAccessoriesPrice("jerseyPrice");
+
+//get the total price 
+const totalPrice = getTotalPrice("totalPrice");
+
+const sum = totalPrice + price;
+const totalPriceEl = document.getElementById("totalPrice");
+totalPriceEl.innerText = sum;
+
+  //   Get the Purchase button ref
+  const purchaseBtn = document.getElementById("purchaseButton");
+  if (sum > 0) {
+    purchaseBtn.classList.remove("btn-disabled");
+    purchaseBtn.classList.add("btn-secondary");
+  }
+  //   Get the Apply button ref
+  const applyBtn = document.getElementById("applyButton");
+  if (sum >= 200) {
+    applyBtn.classList.remove("btn-disabled");
+    applyBtn.classList.add("btn-secondary");
+  }
+}
+
+function handleCardCates() {
+  
+const name = getAccessoriesName("catesName");
+showName("showName",name);
+  
+//get the price of the accessories
+const price = getAccessoriesPrice("catesPrice");
 
 //get the total price 
 const totalPrice = getTotalPrice("totalPrice");
@@ -59,10 +205,7 @@ function handleApplyBtn() {
   const couponConst = "SELL200";
   if (couponValue === couponConst) {
 
-  // get the total price el
-  // const totalPriceEl = document.getElementById("totalPrice");
-  // const totalPriceElString = totalPriceEl.innerText;
-  // const totalPrice = parseFloat(totalPriceElString);
+    // get total price
   const totalPrice = getTotalPrice("totalPrice");
 
     // get the total el
@@ -78,190 +221,28 @@ function handleApplyBtn() {
     //set the total
     totalEl.innerText = totalPrice - disCountTotal;
   }
+  
 }
 
+function resetValue(){
+  // get total price
+  const totalPrice = document.getElementById("totalPrice");
+  totalPrice.textContent = "0";
+  // get the Discount el
+  const discount = document.getElementById("discount");
+  discount.textContent = "0";
+  // get the total el
+  const total = document.getElementById("total");
+  total.textContent = "0";
 
-function handleCardHomeCooker() {
-  let itemCounter = 0;
-const name = getAccessoriesName("cooker");
+  //reset the coupon value
+  document.getElementById("coupon").value= "";
 
-  // get the Ol to show name
-  const showName = document.getElementById("showName");
-  const li = document.createElement("li");
-  li.textContent = `${itemCounter + 1}. ${name}`;
-  li.style.fontWeight = "medium";
-  li.style.fontSize = "24px";
-  li.style.marginBottom = "12px";
-  showName.appendChild(li);
-  itemCounter++;
-//get the price of the accessories
-const price = getAccessoriesPrice("cookerPrice");
+  //reset the ol
+  const ol = document.getElementById("showName");
+  for (const li of addedLi) {
+      ol.removeChild(li);
+    }
+    addedLi.length = 0;
 
-//get the total price 
-const totalPrice = getTotalPrice("totalPrice");
-
-const sum = totalPrice + price;
-const totalPriceEl = document.getElementById("totalPrice");
-totalPriceEl.innerText = sum;
-
-  //   Get the Purchase button ref
-  const purchaseBtn = document.getElementById("purchaseButton");
-  if (sum > 0) {
-    purchaseBtn.classList.remove("btn-disabled");
-    purchaseBtn.classList.add("btn-secondary");
-  }
-  //   Get the Apply button ref
-  const applyBtn = document.getElementById("applyButton");
-  if (sum >= 200) {
-    applyBtn.classList.remove("btn-disabled");
-    applyBtn.classList.add("btn-secondary");
-  }
-}
-
-function handleKAccessories2() {
-  let itemCounter = 0;
-const name = getAccessoriesName("kAccessories2");
-
-  // get the Ol to show name
-  const showName = document.getElementById("showName");
-  const li = document.createElement("li");
-  li.textContent = `${itemCounter + 1}. ${name}`;
-  li.style.fontWeight = "medium";
-  li.style.fontSize = "24px";
-  li.style.marginBottom = "12px";
-  showName.appendChild(li);
-  itemCounter++;
-//get the price of the accessories
-const price = getAccessoriesPrice("kAPrice2");
-
-//get the total price 
-const totalPrice = getTotalPrice("totalPrice");
-
-const sum = totalPrice + price;
-const totalPriceEl = document.getElementById("totalPrice");
-totalPriceEl.innerText = sum;
-
-  //   Get the Purchase button ref
-  const purchaseBtn = document.getElementById("purchaseButton");
-  if (sum > 0) {
-    purchaseBtn.classList.remove("btn-disabled");
-    purchaseBtn.classList.add("btn-secondary");
-  }
-  //   Get the Apply button ref
-  const applyBtn = document.getElementById("applyButton");
-  if (sum >= 200) {
-    applyBtn.classList.remove("btn-disabled");
-    applyBtn.classList.add("btn-secondary");
-  }
-}
-
-function handleCardCap() {
-  let itemCounter = 0;
-const name = getAccessoriesName("capName");
-
-  // get the Ol to show name
-  const showName = document.getElementById("showName");
-  const li = document.createElement("li");
-  li.textContent = `${itemCounter + 1}. ${name}`;
-  li.style.fontWeight = "medium";
-  li.style.fontSize = "24px";
-  li.style.marginBottom = "12px";
-  showName.appendChild(li);
-  itemCounter++;
-//get the price of the accessories
-const price = getAccessoriesPrice("capPrice");
-
-//get the total price 
-const totalPrice = getTotalPrice("totalPrice");
-
-const sum = totalPrice + price;
-const totalPriceEl = document.getElementById("totalPrice");
-totalPriceEl.innerText = sum;
-
-  //   Get the Purchase button ref
-  const purchaseBtn = document.getElementById("purchaseButton");
-  if (sum > 0) {
-    purchaseBtn.classList.remove("btn-disabled");
-    purchaseBtn.classList.add("btn-secondary");
-  }
-  //   Get the Apply button ref
-  const applyBtn = document.getElementById("applyButton");
-  if (sum >= 200) {
-    applyBtn.classList.remove("btn-disabled");
-    applyBtn.classList.add("btn-secondary");
-  }
-}
-
-function handleCardJersey() {
-  let itemCounter = 0;
-const name = getAccessoriesName("jerseyName");
-
-  // get the Ol to show name
-  const showName = document.getElementById("showName");
-  const li = document.createElement("li");
-  li.textContent = `${itemCounter + 1}. ${name}`;
-  li.style.fontWeight = "medium";
-  li.style.fontSize = "24px";
-  li.style.marginBottom = "12px";
-  showName.appendChild(li);
-  itemCounter++;
-//get the price of the accessories
-const price = getAccessoriesPrice("jerseyPrice");
-
-//get the total price 
-const totalPrice = getTotalPrice("totalPrice");
-
-const sum = totalPrice + price;
-const totalPriceEl = document.getElementById("totalPrice");
-totalPriceEl.innerText = sum;
-
-  //   Get the Purchase button ref
-  const purchaseBtn = document.getElementById("purchaseButton");
-  if (sum > 0) {
-    purchaseBtn.classList.remove("btn-disabled");
-    purchaseBtn.classList.add("btn-secondary");
-  }
-  //   Get the Apply button ref
-  const applyBtn = document.getElementById("applyButton");
-  if (sum >= 200) {
-    applyBtn.classList.remove("btn-disabled");
-    applyBtn.classList.add("btn-secondary");
-  }
-}
-
-function handleCardCates() {
-  let itemCounter = 0;
-const name = getAccessoriesName("catesName");
-
-  // get the Ol to show name
-  const showName = document.getElementById("showName");
-  const li = document.createElement("li");
-  li.textContent = `${itemCounter + 1}. ${name}`;
-  li.style.fontWeight = "medium";
-  li.style.fontSize = "24px";
-  li.style.marginBottom = "12px";
-  showName.appendChild(li);
-  itemCounter++;
-//get the price of the accessories
-const price = getAccessoriesPrice("catesPrice");
-
-//get the total price 
-const totalPrice = getTotalPrice("totalPrice");
-
-const sum = totalPrice + price;
-const totalPriceEl = document.getElementById("totalPrice");
-totalPriceEl.innerText = sum;
-
-  //   Get the Purchase button ref
-  const purchaseBtn = document.getElementById("purchaseButton");
-  if (sum > 0) {
-    purchaseBtn.classList.remove("btn-disabled");
-    purchaseBtn.classList.add("btn-secondary");
-  }
-  //   Get the Apply button ref
-  const applyBtn = document.getElementById("applyButton");
-  if (sum >= 200) {
-    applyBtn.classList.remove("btn-disabled");
-    applyBtn.classList.add("btn-secondary");
-  }
 }
